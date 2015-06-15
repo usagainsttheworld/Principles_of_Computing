7eb0517320511d68af2ce13c0b43bdfdbedab933
Ling Tian <sweetbellor.ling@gmail.com>
2015-06-14 23:39:01 -0400

    score updating,bug with best move needs fix

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -12,8 +12,15 @@ import poc_ttt_provided as provided
 NTRIALS = 1         # Number of trials to run
 SCORE_CURRENT = 1.0 # Score for squares played by the current player
 SCORE_OTHER = 1.0   # Score for squares played by the other player
+
+def build_scorce_grid(dim):
+    score_list = []
+    for row in range (dim):
+        for col in range(dim):
+            element = [row, col, 0]
+            score_list.append(element)
+    return score_list
     
-# Add your functions here.
 def mc_trial(board, player):
     """
     this function play a game starting with the given player by making 
@@ -32,8 +39,29 @@ def mc_update_scores(scores, board, player):
     same dimensions as the Tic-Tac-Toe board, a board from a 
     completed game, and which player the machine player is. 
     The function score the completed board and update the scores grid.
-    """   
-    
+    """ 
+    for dummy_square in scores:
+        if board.square(dummy_square[0], dummy_square[1]) == board.check_win():        
+            dummy_square[2] += 1
+        elif board.square(dummy_square[0], dummy_square[1]) == 5-board.check_win():
+            dummy_square[2] -= 1  
+
+def get_best_move(board, scores):
+    """
+    The function find all of the empty squares with the maximum score and 
+    randomly return one of them as a (row, column) tuple. It is an error to 
+    call this function with a board that has no empty squares (there is no possible 
+    next move), so your function may do whatever it wants in that case. 
+    The case where the board is full will not be tested.
+    """
+    highest_so_far = -1
+    highest_list = []
+    for dummy_square in scores:
+        if board.square(dummy_square[0], dummy_square[1]) == 1 and dummy_square[2] >= highest_so_far:
+            highest_so_far = dummy_square[2]
+            highest_list.append((dummy_square[0], dummy_square[1]))        
+    return highest_list          
+     
 # Test game with the console or the GUI.  Uncomment whichever 
 # you prefer.  Both should be commented out when you submit 
 # for testing to save time.
@@ -54,12 +82,18 @@ print game.get_empty_squares()
 print "position 0, 0 is", game.square(0,0)
 print game.check_win()
 print "============="
+
 game = provided.TTTBoard(3,False, None)
 mc_trial(game, 2)
 print game.__str__()
 print "the winner is", game.check_win()
+print "============="
+game_scorce_grid = build_scorce_grid(3)
+print game_scorce_grid
+mc_update_scores(game_scorce_grid, game, 2)
+print game_scorce_grid
+print"============="
 
 
-
-
-
+game_new = provided.TTTBoard(3,False, None)
+print get_best_move(game_new, game_scorce_grid)
